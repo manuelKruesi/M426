27996779058a24614258d0889309055f03afacff
Manuel Krüsi <manu@zero-one.cc>
2019-05-21 11:37:46 +0200

    setting up the reservation Controller

---
+++ app/Http/Controllers/ReservationController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReservationController extends Controller
+{
+  /**
+  * Handles the reservations
+  *
+  * @param  int  $id
+  * @return Response
+  */
+  public function handleReservation($id)
+  {
+    switch ($id) {
+      case 1:
+        // code...
+        break;
+
+      case 2:
+        // code
+        break;
+
+      default:
+        // code...
+        break;
+    }
+  }
+}
